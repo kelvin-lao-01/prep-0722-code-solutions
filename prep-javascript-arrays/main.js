b58f0407d8756b6054df6b25831341352ad3b221
Kelvin Lao <kelvinlaoo01@yahoo.com>
2022-08-09 23:15:23 +0000

    Completed prep-javascript-arrays exercise

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log(languages);
+
+const firstElement = 'html';
+console.log('The first element of the array is', firstElement);
+
+const thirdElement = 'javascript';
+console.log('The third element of the array is', thirdElement);
+
+const length = '6';
+console.log('The length of the array is', length);
+
+const lastElement = 'postgres';
+console.log('The last element of the array is', lastElement);
