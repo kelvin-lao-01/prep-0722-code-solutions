cfc7e5b6c897809452c0588bb8597ed18116d37a
Kelvin Lao <kelvinlaoo01@yahoo.com>
2022-08-10 20:25:42 +0000

    made changes

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,14 @@
-const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log(languages);
 
-var firstElement = 'html';
+var firstElement = languages(0);
 console.log('The first element of the array is', firstElement);
 
-var thirdElement = 'javascript';
+var thirdElement = languages(3);
 console.log('The third element of the array is', thirdElement);
 
-var length = '6';
+var length = languages.length;
 console.log('The length of the array is', length);
 
-var lastElement = 'postgres';
+var lastElement = languages(6);
 console.log('The last element of the array is', lastElement);
