7c799533dd97921c7c55f903b7ea7405f8adc116
Kelvin Lao <kelvinlaoo01@yahoo.com>
2022-08-10 03:43:25 +0000

    Completed prep-javascript-arrays exercise

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,14 @@
 const languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log(languages);
 
-const firstElement = 'html';
+var firstElement = 'html';
 console.log('The first element of the array is', firstElement);
 
-const thirdElement = 'javascript';
+var thirdElement = 'javascript';
 console.log('The third element of the array is', thirdElement);
 
-const length = '6';
+var length = '6';
 console.log('The length of the array is', length);
 
-const lastElement = 'postgres';
+var lastElement = 'postgres';
 console.log('The last element of the array is', lastElement);
