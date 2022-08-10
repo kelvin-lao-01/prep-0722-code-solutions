623575fcd03b4599ad1872cc30daa780805ea950
Kelvin Lao <kelvinlaoo01@yahoo.com>
2022-08-10 18:30:26 +0000

    Completed prep-javascript-for-loops exercise

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -6,5 +6,5 @@ for (let i = 0; i < 10; i++) {
   console.log(2 * i);
 }
 for (let i = 0; i < 100; i++) {
-  console.log('Time till explosion', 99 - i);
+  console.log('Time till explosion', 100 - i);
 }
