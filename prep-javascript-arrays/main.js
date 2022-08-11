c4c4776b9ab0521b0dcd944d957cec37ac0b8681
Kelvin Lao <kelvinlaoo01@yahoo.com>
2022-08-11 00:48:56 +0000

    Added brackets

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -1,14 +1,14 @@
 var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
 console.log(languages);
 
-var firstElement = languages(0);
+var firstElement = languages[0];
 console.log('The first element of the array is', firstElement);
 
-var thirdElement = languages(3);
+var thirdElement = languages[3];
 console.log('The third element of the array is', thirdElement);
 
 var length = languages.length;
 console.log('The length of the array is', length);
 
-var lastElement = languages(6);
+var lastElement = languages[6];
 console.log('The last element of the array is', lastElement);
